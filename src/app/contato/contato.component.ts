966177fb2b2e37031df0f4a7355f9a0e7b3d727a
Thayscosta95 <tha_tacosta@hotmail.com>
2025-07-01 18:24:30 -0300

    portfolio em angular

---
+++ src/app/contato/contato.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato',
+  imports: [],
+  templateUrl: './contato.component.html',
+  styleUrl: './contato.component.css'
+})
+export class ContatoComponent {
+
+}
